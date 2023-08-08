6b069a11820560a30a804898ecb5ca59b4f06fcc
nicodemos.junior <nicodemos.junior@dataprev.gov.br>
2023-08-08 09:20:15 -0300

    Bugfix CORS

--- restful-api-santander-dev-week/src/main/java/com/example/Application.java
+++ restful-api-santander-dev-week/src/main/java/com/example/Application.java
@@ -3,6 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url  = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class Application {
 
